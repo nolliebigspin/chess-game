73cb128b317ed19dc9b71a28ff3a88df17546655
Alec Winter <alec.winter@student.uni-luebeck.de>
2020-04-09 21:28:14 +0200

    added entry question of game

--- src/main/java/schach/Main.java
+++ src/main/java/schach/Main.java
@@ -2,6 +2,7 @@ package schach;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is a chess Game!");
+        System.out.println("This is a chess game!");
+        System.out.println("Please enter if you want to play against another human or against our artificial intelligence?");
     }
 }
