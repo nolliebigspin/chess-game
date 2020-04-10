379cfd476a335aa954d3ad96872014313c3b1d07
Alec Winter <alec.winter@student.uni-luebeck.de>
2020-04-10 13:29:20 +0200

    removed empty line

--- src/main/java/schach/Main.java
+++ src/main/java/schach/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is a chess game!");
         System.out.println("Please enter if you want to play against another human or against our artificial intelligence?");
-
     }
 }
