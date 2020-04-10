a7bfe6ea3521e02cd54400e87dae2b5b0b04bed4
Alec Winter <alec.winter@student.uni-luebeck.de>
2020-04-10 13:26:07 +0200

    fix merge conflict

--- src/main/java/schach/Main.java
+++ src/main/java/schach/Main.java
@@ -2,11 +2,8 @@ package schach;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("This is a chess Game!!!");
-=======
         System.out.println("This is a chess game!");
         System.out.println("Please enter if you want to play against another human or against our artificial intelligence?");
->>>>>>> 73cb128b317ed19dc9b71a28ff3a88df17546655
+
     }
 }
