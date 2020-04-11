ad71bf349514d93e2944db7e9bf3b8491a764b58
Heinrich Stremme <Heinrich.Stremme@Student.uni-luebeck.de>
2020-04-11 23:22:46 +0200

    main class updated

--- src/main/java/schach/Main.java
+++ src/main/java/schach/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is a chess game!");
         System.out.println("Please enter if you want to play against another human or against our artificial intelligence?");
+        Board board = new Board();
+        board.printBoard();
+        Input input = new Input(board);
+        input.inOutRoutine();
     }
 }
