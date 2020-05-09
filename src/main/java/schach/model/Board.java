c646290cc81056442e2d726b15158406e99524d8
Heinrich Stremme <Heinrich.Stremme@Student.uni-luebeck.de>
2020-05-09 22:23:20 +0200

    resolved PMD MethodReturnsInternalArray by deleting get squareMatrix() method in Board Class

--- src/main/java/schach/model/Board.java
+++ src/main/java/schach/model/Board.java
@@ -10,10 +10,6 @@ import java.util.List;
  */
 public class Board {
 
-    public Square[][] getSquareMatrix() {
-        return squareMatrix;
-    }
-
     /**
      * Matrix of Squares representing the chess board
      */
