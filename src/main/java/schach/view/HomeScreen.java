7ec2ef4b0efae59c051df33518f4037e945f3da5
yahya <yah.alsamoualnajjar@student.uni-luebeck.de>
2020-06-08 14:10:43 +0200

    lil things and stuff

--- src/main/java/schach/view/HomeScreen.java
+++ src/main/java/schach/view/HomeScreen.java
@@ -1,8 +1,7 @@
 package schach.view;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.SynchronousQueue;
+
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -171,8 +170,8 @@ public class HomeScreen extends Pane {
 		HBox boardBox = new HBox();
 		this.boardPane = new GridPane();
 		this.boardPane.setPrefSize(600, 600);
-		updateBoard();
-		//resetBoard(this.board);
+		//updateBoard();
+		reset();
 		boardBox.getChildren().add(boardPane);
 		content.setPrefSize(700, 500);
 		/// Last Move section
@@ -219,6 +218,8 @@ public class HomeScreen extends Pane {
 						@Override
 						public void handle(MouseEvent e) {
 							Square tile = board.getSquares()[finalCol][7 - finalRow];
+							squareTile.setBackground(new Background(new BackgroundFill(Color.RED, null, Insets.EMPTY)));
+
 							final Piece occupier = tile.getOccupier();
 
 							if (occupier.isWhite()) {
@@ -227,7 +228,7 @@ public class HomeScreen extends Pane {
 								//resetBoard();--------------------------------------------------------------------
 								for (final Square s : legal) {
 									//Highlight possible moves if turned on
-									squareTile.setBackground(new Background(new BackgroundFill(Color.RED, null, Insets.EMPTY)));
+
 									for (int i = 0; i < occupier.filteredLegals().size(); i++) {
 										System.out.println("can move to:" + occupier.filteredLegals().get(i).getColumn() + ", " + occupier.filteredLegals().get(i).getRow());
 
@@ -250,4 +251,77 @@ public class HomeScreen extends Pane {
 			}
 		}
 	}
+
+
+
+	private final Map<Label, Square> map = new HashMap<Label, Square>();
+	private void reset() {
+		Color squareColor;
+
+		for (int col = 0; col < 8; col++) {
+			for (int row = 0; row < 8; row++) {
+				if ((row + col) % 2 == 0) {
+					squareColor = Color.BLACK;
+				} else {
+					squareColor = Color.GREY;
+				}
+				labels[col][row] = new Label(col + "," + (row));
+
+				labels[col][row].prefWidthProperty().bind(boardPane.widthProperty().divide(8));
+				labels[col][row].prefHeightProperty().bind(boardPane.heightProperty().divide(8));
+				GridPane.setFillWidth(labels[col][row], true);
+				GridPane.setFillHeight(labels[col][row], true);
+
+				if (board.getSquares()[col][row].isOccupied()) {
+//							Label b = new Label("  " + board.getSquares()[col - 1][row].getOccupier().print());
+					Label squareTile = new Label("     " + (board.getSquares()[col][row].getOccupier().print()));
+					squareTile.prefHeightProperty().bind(boardPane.heightProperty().divide(8));
+					squareTile.prefWidthProperty().bind(boardPane.widthProperty().divide(8));
+					// color
+					squareTile.setBackground(new Background(new BackgroundFill(squareColor, null, Insets.EMPTY)));
+					boardPane.add(squareTile, col, row);
+					map.put(squareTile, new Square(col, row));
+					// Add listener
+					int finalCol = col;
+					int finalRow = row;
+					final Label temp = labels[col][row];
+					squareTile.setOnMouseClicked(new EventHandler<MouseEvent>() {
+						@Override
+						public void handle(MouseEvent e) {
+							Square ts = board.getSquares()[finalCol][7 - finalRow];
+							squareTile.setBackground(new Background(new BackgroundFill(Color.RED, null, Insets.EMPTY)));
+							final Piece occupier = ts.getOccupier();
+							final List<Square> legal = occupier.filteredLegals();
+
+								  //resetBoard();--------------------------------------------------------------------
+								   for (final Square s : legal) {
+									//Highlight possible moves if turned on
+
+									for (int i = 0; i < occupier.filteredLegals().size(); i++) {
+										System.out.println("can move to:" + occupier.filteredLegals().get(i).getColumn() + ", " + occupier.filteredLegals().get(i).getRow());
+										System.out.println(ts);
+										System.out.println(map.get(squareTile));
+									}
+
+								}
+
+						}
+
+					});
+
+				} else {
+					Label temp = labels[col][row];
+					Label squareTile = new Label();
+					squareTile.setContentDisplay(ContentDisplay.CENTER);
+					squareTile.prefHeightProperty().bind(boardPane.heightProperty().divide(8));
+					squareTile.prefWidthProperty().bind(boardPane.widthProperty().divide(8));
+					squareTile.setBackground(new Background(new BackgroundFill(squareColor, null, Insets.EMPTY)));
+					boardPane.add(squareTile, col, row);
+					map.put(squareTile, new Square(col, row));
+				}
+			}
+		}
+	}
+
+
 }
